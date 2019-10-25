132a473b99a195acea075d0523bbb914a1bc1a4e
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 00:04:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size <= 1 
+    return array.join          
+  elsif array.size == 2 
+    return array.join(" and ")
+  else 
+    p array          #returns array          
+    array.pop  
+    
+    p array 
+    sentence = array.join(", ")
+    p sentence
+    sentence2 = sentence << ", and #{add_array}"
+    return sentence2 
+  end
 end
+
